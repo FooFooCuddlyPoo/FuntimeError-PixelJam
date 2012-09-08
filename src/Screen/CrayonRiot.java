31033765329c4f93dd72f493b1ee396b20776d34
Craig <craig@forret.net>
2012-09-08 12:39:37 +1200

    made screen package and stuff

---
+++ src/Screen/CrayonRiot.java
@@ -0,0 +1,25 @@
+package Screen;
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+
+@SuppressWarnings("serial")
+public class CrayonRiot extends JFrame {
+
+	 
+	public CrayonRiot(){
+	    
+		ScreenPanel sc = new ScreenPanel();
+		add(sc);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+	  
+	}
+
+}
